f7bdcbcc6b7c656afa9503b8a5ad65355f785f3b
Flavio Reis <flavioreissouza@yahoo.com>
2023-06-03 12:03:20 -0300

    Create entity and configuring the H2 database

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,41 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String phone;
+
+    private LocalDate birthDate;
+
+    private String password;
+}
