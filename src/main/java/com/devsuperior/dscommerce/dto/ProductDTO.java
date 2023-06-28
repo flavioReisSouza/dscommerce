850cf21a68dbd8f9c2fd69f25233b72af29dfb25
Flavio Reis <flavioreissouza@yahoo.com>
2023-06-27 21:31:53 -0300

    Created controller, repository, dto and service

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,30 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ProductDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private String imgUrl;
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+}
