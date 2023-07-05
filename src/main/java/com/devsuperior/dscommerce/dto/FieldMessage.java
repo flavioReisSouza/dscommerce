b62b6cd12b55a00ac9430db822f75b6f445980ec
Flavio Reis <flavioreissouza@yahoo.com>
2023-07-04 21:06:24 -0300

    Bean Validation and refactoring exception handling

---
+++ src/main/java/com/devsuperior/dscommerce/dto/FieldMessage.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class FieldMessage {
+
+    private String fieldName;
+
+    private String message;
+}
--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,6 +1,9 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,9 +12,17 @@ import lombok.Getter;
 public class ProductDTO {
 
     private Long id;
+
+    @NotBlank(message = "Required field")
+    @Size(min = 3, max = 80 , message = "The name must be between 3 and 80 characters")
     private String name;
+
+    @Size(min = 10 , message = "The description must be at least 10 characters")
     private String description;
+
+    @Positive(message = "The price must be a positive value")
     private Double price;
+
     private String imgUrl;
 
     public ProductDTO(Product entity) {
