a2f45219450f4a06ed0351e93296872f80f1f670
Flavio Reis <ext.flavios@unicid.edu.br>
2024-01-02 11:26:05 -0300

    Add Endpoint Categories

---
+++ src/main/java/com/devsuperior/dscommerce/services/CategoryService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.CategoryDTO;
+import com.devsuperior.dscommerce.entities.Category;
+import com.devsuperior.dscommerce.repositories.CategoryRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<CategoryDTO> findAll() {
+        List<Category> result = categoryRepository.findAll();
+        return result.stream().map(CategoryDTO::new).toList();
+    }
+}
